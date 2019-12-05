7b367e55fe3301e6234b68cda777a164aee10650
Bodo Alexander Voigt <h_o_p_e_l_e_s_s_n_e_s_s@msn.com>
2019-12-05 12:05:26 +0100

    updated MPaths.py so that main.py can be started from anywhere and not just from src directory

--- src/MPaths.py
+++ src/MPaths.py
@@ -1,10 +1,13 @@
 ########################################################################
 #%% import block     ###################################################
 ########################################################################
-from os.path import join
+from os.path import dirname, abspath, join
 ########################################################################
 #%% set paths to files     #############################################
 ########################################################################
-datadir = "../data"
-playerlistpath = join(datadir,"players.json")
-playerdir      = join(datadir,"players")
+repodir = dirname(dirname(abspath(__file__)))
+datadir = join(repodir, "data")
+playerlistpath = join(datadir, "players.json")
+playerdir      = join(datadir, "players")
+readmemdpath   = join(repodir, "README.md")
+readmepath     = join(repodir, "readme")
